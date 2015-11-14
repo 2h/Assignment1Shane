f56718cec5095198db57a3507407f6f57c54ebea
2h <Hugh.Healy@gmail.com>
2015-11-14 09:01:03 +0000

    Sat14ZoneDetection

--- Unity/New Unity Project/Assets/Scripts/detectionZone.cs
+++ Unity/New Unity Project/Assets/Scripts/detectionZone.cs
@@ -1,26 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class detectionZone : MonoBehaviour {
-			
-	void onTriggerEnter2D(Collider2D other)
+public class DetectionZone : MonoBehaviour {
+	
+	void OnTriggerEnter2D(Collider2D other)
 	{
-		Debug.Log("Object Entered the Trigger");
+		Debug.Log("Object Entered the Trigger: " + other.gameObject.tag);
 	}
 	
 	void onTriggerStay2D(Collider2D other)
 	{
-		Debug.Log("Object is within the Trigger");
+		Debug.Log("Object is within the Trigger" + other.gameObject.tag);
 	}
 	
 	void onTriggerExit2D(Collider2D other)
 	{
-		Debug.Log ("Objext exited the Trigger");
+		Debug.Log ("Objext exited the Trigger" + other.gameObject.tag);
 	}
-
-//		if(thingWeCollidedWith.gameObject.CompareTag("Player"))
-//		{
-//			transform.position = Vector2.MoveTowards (transform.position, thingWeCollidedWith.transform.position, speed * Time.deltaTime);
-//		}
-
 }
